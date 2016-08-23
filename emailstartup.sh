e3d8fa7ec09a21c3e80a1ceeaee5dd9f20e4cba4
I2obiN <thomashood10@yahoo.com>
2016-08-23 15:04:11 +0100

    Update and rename startup.sh to emailstartup.sh
    Added comment line re yahoo mail

--- emailstartup.sh
+++ emailstartup.sh
@@ -3,6 +3,7 @@
 # Startup script by I2obiN
 # Requires mutt setup -- sends your WAN IP to an email address
 # Add to crontab -e @reboot or for Ubuntu search "startup applications" and add the script file
+# NB: Yahoo will not let you send smtp emails from your free account, use gmail
 
 curl -s http://whatismijnip.nl |cut -d " " -f 5 | mutt -s "Your Subject" youremail@yahoo.com
 # endline
