5e14136580addccb255edec8b47690a9c3ded752
I2obiN <thomashood10@yahoo.com>
2016-10-10 03:08:19 +0100

    Update macswap.sh

--- macswap.sh
+++ macswap.sh
@@ -3,7 +3,7 @@
 # Bring wireless device down
 ifconfig wlan0 down
 
-# Swap mac to random address
+# Swap mac to random address, if one a wired connection change 'wlan0' to 'eth0'
 macchanger -a wlan0
 
 # Bring wireless device up
